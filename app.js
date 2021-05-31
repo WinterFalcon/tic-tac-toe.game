a7b3c636be50f6e099c68849c0653d02f431f557
WinterFalcon <84065150+WinterFalcon@users.noreply.github.com>
2021-05-31 12:58:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const boxes =Array.from(document.getElementsByClassName('box'));
+const playText = document.getElementById('playText');
+const spaces = [null,null,null,null,null,null,null,null,null];
+const X_TEXT = "X";
+const O_TEXT = "0";
+let currentPlayer= X_TEXT;
+console.log(boxes);
+
+const drawBoard = () => {
+    boxes.forEach((box, index) => {
+      let styleString = '';
+      if (index < 3) {
+          styleString += 'border-bottom: 3px solid #080808;';
+      }
+      if (index % 3 === 0){
+          styleString += 'border-right: 3px solid #080808;';
+      }
+      if (index % 3 === 2){
+        styleString += 'border-left: 3px solid #080808;';
+    }
+      if (index > 5) {
+        styleString += 'border-top: 3px solid #080808;';
+    }
+    box.style = styleString;
+    box.addEventListener('click', boxClicked);
+    });
+
+};
+
+const boxClicked = (e) => {
+    const id = e.target.id;
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if(playerHasWon()){
+            playText.innerText = '${currentPlayer} has won!';
+            return;
+        }
+
+        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+    }
+};
+
+const playerHasWon = () => {
+    if(spaces[0] === currentPlayer) {
+        if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+            console.log('${currentPlayer} wins up top');
+            return true;
+        }
+        if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+            console.log('${currentPlayer} wins on the left');
+            return true;
+        }
+        if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+            console.log('${currentPlayer} wins diagnolly');
+            return true;
+        }
+    }
+ }
+
+
+drawBoard();
